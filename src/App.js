2015f3d77eccdb7b1e93baf3c79cbb9964c95a2b
caiosdf <caiosdf15@gmail.com>
2020-12-01 14:53:04 -0300

    + seção sinos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from '../src/components/Navbar/Navbar';
 import ArteDeTodaGente from '../src/views/ArteDeTodaGenteSec/ArteDeTodaGenteSec';
+import Sinos from '../src/views/SinosSec/SinosSec';
 
 function App() {
   return (
     <>
       <Navbar />
       <ArteDeTodaGente />
+      <Sinos />
     </>
   );
 }
---
+++ src/views/SinosSec/SinosSec.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './SinosSec.css'
+
+import logo from '../../assets/img/logo-sinos.png';
+import flautista from '../../assets/img/flautista.png';
+
+function SinosSec(){
+
+    return(
+        <>
+            <section className="full-section-sinos">
+                <section className="red-section"></section>
+                <section className="sinos-container">
+                    <section className="col-1">
+                        <img className="logo-sinos" src={logo}/>
+                        <p className="text-section-sinos">O Sinos é sustentado por uma rede de dezenas de profissionais de música, que atuam em cursos, oficinas, concertos e festivais. O objetivo é capacitar regentes, instrumentistas, compositores e educadores, apoiando projetos sociais de música, e contribuir para o desenvolvimento das orquestras-escola do Brasil. Em 2020, serão 472 videoaulas, com 188 horas de conteúdo.</p>
+                        <a className="button" href="https://sinos.art.br/">VISITE O SITE</a>
+                    </section>
+                    <section className="col-2">
+                        <img className="img-flautista" src={flautista}/>
+                    </section>
+                </section>
+            </section>
+        </>
+    );
+
+}
+
+export default SinosSec;
