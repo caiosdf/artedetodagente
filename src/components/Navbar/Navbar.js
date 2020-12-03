c0dfc824d80b8474881cfca42c2ec8c34e8c9300
Caio Silva <caiosdf15@gmail.com>
2020-12-03 17:29:52 -0300

    ajuste navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,8 +5,8 @@ function Navbar(){
     return(
         <div className="navbar">
             <div className="navbar-items-container">
-                <a className="navbar-item">Agenda</a>
-                <div className="navbar-dot"></div>
+                {/* <a className="navbar-item">Agenda</a>
+                <div className="navbar-dot"></div> */}
                 <a className="navbar-item" href="#sinos">Sinos</a>
                 <div className="navbar-dot"></div>
                 <a className="navbar-item" href="#bossa">Bossa Criativa</a>
