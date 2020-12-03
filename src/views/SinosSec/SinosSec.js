c0338d612a76b29f58659ada01855ecd1887c5c8
Caio Silva <caiosdf15@gmail.com>
2020-12-03 17:08:21 -0300

    ajuste nos alt's das imagens

--- src/views/SinosSec/SinosSec.js
+++ src/views/SinosSec/SinosSec.js
@@ -12,12 +12,12 @@ function SinosSec(){
                 <section className="red-section"></section>
                 <section className="sinos-container">
                     <section className="col-1">
-                        <img className="logo-sinos" src={logo}/>
+                        <img className="logo-sinos" src={logo} alt="logo sinos"/>
                         <p className="text-section">O Sinos é sustentado por uma rede de dezenas de profissionais de música, que atuam em cursos, oficinas, concertos e festivais. O objetivo é capacitar regentes, instrumentistas, compositores e educadores, apoiando projetos sociais de música, e contribuir para o desenvolvimento das orquestras-escola do Brasil. Em 2020, serão 472 videoaulas, com 188 horas de conteúdo.</p>
                         <a className="button" href="https://sinos.art.br/">VISITE O SITE</a>
                     </section>
                     <section className="col-2">
-                        <img className="img-flautista" src={flautista}/>
+                        <img className="img-flautista" src={flautista} alt="pessoa tocando flauta"/>
                     </section>
                 </section>
             </section>
--- src/views/UnoSec/UnoSec.js
+++ src/views/UnoSec/UnoSec.js
@@ -8,12 +8,12 @@ function Uno(){
     return(
         <section className="full-section-uno">
             <div className="col-1-uno">
-                <img className="logo-uno" src={logo}/>
+                <img className="logo-uno" src={logo} alt="logo um novo olhar"/>
                 <p className="text-section">O Um Novo Olhar reúne capacitações em arte-educação e em regência coral, para promover a arte como forma de inclusão e acesso de crianças, jovens e adultos com deficiência. Ao longo de 2020, mais de 70 artistas e educadores se dividem entre cerca de 200 horas de performances, apresentações e capacitações, além de publicações, palestras e eventos com especialistas de diversos países.</p>
                 <a className="button" href="https://umnovoolhar.art.br/">VISITE O SITE</a>
             </div>
             <div className="col-2-uno">
-                <img className="img-uno" src={imgUno}/>
+                <img className="img-uno" src={imgUno} alt=""/>
             </div>
         </section>
     );
