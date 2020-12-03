97612128f9fb66935370924744e0ea103d7481b5
Caio Silva <caiosdf15@gmail.com>
2020-12-03 17:13:36 -0300

    ajuste nos alt's das imagens

--- src/views/ArteDeTodaGenteSec/ArteDeTodaGenteSec.js
+++ src/views/ArteDeTodaGenteSec/ArteDeTodaGenteSec.js
@@ -9,18 +9,18 @@ function ArteDeTodaGente(){
     return(
         <section className="full-section">      
             <section className="logo-adtg">
-                <img src={logo}></img>
+                <img src={logo} alt="logo do projeto arte de toda gente"></img>
             </section>
             <section className="adtg-content">
                 <div className="text-container">
                     <p className="text-section">O Arte de Toda Gente engloba os projetos Bossa Criativa – Arte de Toda Gente, Um Novo Olhar e Sistema Nacional de Orquestras Sociais – Sinos; frutos da parceria entre a Fundação Nacional da Artes - Funarte e a Universidade Federal do Rio de Janeiro - UFRJ, com a curadoria de sua Escola de Música, envolvendo artistas e educadores, compartilhando cultura e conhecimento.</p>
-                    <a className="button" href="https://www.google.com">Conheça os projetos</a>
+                    <a className="button">Conheça os projetos</a>
                 </div>
                 <section className="video-frame-container">
                     <YouEmbed url={'https://youtu.be/qGY4rgFvQCU'} />
                     <div className="video-frame-footer">
                         <a className="youtube-button" href="https://www.youtube.com/channel/UC5RwQ_KlODEtsnK4mRufQNg" target="blank">
-                            <img src={icoYoutube}></img>
+                            <img src={icoYoutube} alt="logo youtube"></img>
                             <p>INSCREVA-SE</p>
                         </a>
                         <p>NO CANAL ARTE DE TODA GENTE</p>
