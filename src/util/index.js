28c3f943dd64f6a8dd772b623aef3ba0131e8c9e
Caio Silva <caiosdf15@gmail.com>
2020-12-10 12:42:19 -0300

    scroll mais lento

--- src/util/index.js
+++ src/util/index.js
@@ -3,6 +3,11 @@ const fdate = (d) => {
     const dateTimeFormat = new Intl.DateTimeFormat('pt-BR', { year: 'numeric', month: 'long', day: '2-digit', timeZone: 'UTC' })
     const [{ value: day },,{ value: month },,{ value: year }] = dateTimeFormat.formatToParts(date) 
     return {day,month,year}
-  }
+}
+
+function scroll(Id){
+  const element = document.querySelector(Id);
+  element.scrollIntoView({behavior: 'smooth'});
+}
   
-  export {fdate}
+export {fdate, scroll}
